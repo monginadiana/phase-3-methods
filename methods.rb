f48f172aeebbb43c43ce9000c6cc7d03594caea8
monginadiana <dianahmongina2@gmail.com>
2022-09-21 11:21:47 +0300

    commit tree 024ba4b28941ab29449c2b41549fabe2436c3c87

--- methods.rb
+++ methods.rb
@@ -1,1 +1,37 @@
 # Your code here!
+
+def greet_programmer
+    puts "Hello, programmer!"
+    "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+    "Hello, #{name}!"
+end
+greet('Naureen!')
+
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+    "Hello, #{name}!"
+
+end
+
+greet_with_default('Naureen!')
+
+
+
+def add(x,y)
+    return x + y 
+end
+
+def halve(number=2)
+    if(number.class != Integer) 
+        return nil
+    end
+    number/2 
+    
+end
+
+halve()
